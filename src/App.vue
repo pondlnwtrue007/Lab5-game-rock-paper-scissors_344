445c498e92b331280b9db92b13aef063156b4dce
tamma <koonkoonpond2@gmail.com>
2023-08-29 03:21:33 +0700

    Web

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>เป่า ยิ้ง ฉุบ</h1>
+    <button @click="playGame('rock')">ค้อน</button>
+    <button @click="playGame('paper')">กระดาษ</button>
+    <button @click="playGame('scissors')">กรรไกร</button>
+    <img :src="playerImgUrl" alt="Player choice" />
+    <img :src="computerImgUrl" alt="Computer choice" />
+    <p>{{ result }}</p>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const playerImgUrl = ref(new URL('./assets/default.png', import.meta.url).href);
+const computerImgUrl = ref(new URL('./assets/default.png', import.meta.url).href);
+const result = ref('');
+
+const choices = {
+  rock: './assets/rock.png',
+  paper: './assets/paper.png',
+  scissors: './assets/scissors.png',
+};
+
+const playGame = (playerChoice) => {
+  playerImgUrl.value = new URL(choices[playerChoice], import.meta.url).href;
+  
+  const computerChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+  computerImgUrl.value = new URL(choices[computerChoice], import.meta.url).href;
+  
+  if (playerChoice === computerChoice) {
+    result.value = 'Draw!';
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result.value = 'You win!';
+  } else {
+    result.value = 'You lose!';
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
